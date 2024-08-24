c63ea72e14bb679af09acbf62432c3086b1bd9c4
Marius Leblanc <marius0leblanc@gmail.com>
2024-08-24 19:05:44 +0200

    Modified admin site

--- src/SnakesAndIAs/settings.py
+++ src/SnakesAndIAs/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'SnakesAndIAs.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
